cb2f9007e10d8ad605129afa610826ed51c09b9e
Kauealvs <kauealvesdesena@gmail.com>
2021-06-16 20:30:08 -0300

    aula dia 16/06/2021

--- exemplos-aulas/classes-e-atributos/index.js
+++ exemplos-aulas/classes-e-atributos/index.js
@@ -1,4 +1,20 @@
 const menu = document.querySelector(".menu");
-console.log(menu.className);
 
-console.log(menu);
+menu.classList.add("nova-classe");
+menu.classList.remove("p");
+menu.classList.toggle("p");
+menu.classList.toggle("p");
+const contemClasse = menu.classList.contains("nova-clas");
+
+if (menu.classList.contains("nova-clase")) {
+	menu.classList.add("adicionei-uma-nova-classe-com-contains");
+	console.log(menu.classList.value);
+} else {
+	menu.classList.remove("vtex");
+}
+
+menu.classList = `class="ativo"`;
+
+// console.log(menu.classList.value);
+
+console.log(menu.attributes["class"]);
