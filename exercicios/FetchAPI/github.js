c415ad6260d1b041a879555360fea468d3ab6b50
Kauealvs <kauealvesdesena@gmail.com>
2021-07-31 12:36:57 -0300

    aula 31/07/2021

---
+++ exercicios/FetchAPI/github.js
@@ -0,0 +1,7 @@
+const url = "https://api.github.com/users";
+
+//criar um pesquisa de usuários do github
+
+//precisa ter uma barra que busque pelo usuário e faça a requisição
+
+//deve trazer o avatar_url, login, followers, following, public_repos, name
