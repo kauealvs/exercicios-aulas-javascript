7508d678c5bfbe24793f0c9b4e835b5e5eaf8ae3
fnicom <fnicomedes@gmail.com>
2021-08-05 14:11:34 -0300

    fix githubApi

--- exercicios/FetchAPI/apiGitHub/github.js
+++ exercicios/FetchAPI/apiGitHub/github.js
@@ -1,7 +1,40 @@
-const url = "https://api.github.com/users";
-
 //criar um pesquisa de usuários do github
 
 //precisa ter uma barra que busque pelo usuário e faça a requisição
 
 //deve trazer o avatar_url, login, followers, following, public_repos, name
+
+
+
+let icon = document.querySelector(".avatar")
+
+const gitHubApi = () => {
+    let searchUser = document.querySelector(".search").value;
+    let urlGit = `https://api.github.com/users/${searchUser}`;
+
+    fetch(urlGit).then(r => r.json()).then(response => {
+        console.log(response)
+        console.log(urlGit)
+        
+
+        document.querySelector(".avatar").innerHTML = response.avatar_url
+
+        icon.setAttribute("src", `${response.avatar_url}`)
+
+        document.querySelector(".login").innerHTML = response.login
+
+        document.querySelector(".location").innerHTML = response.location
+       
+        document.querySelector(".followers").innerHTML = response.followers
+
+        document.querySelector(".following").innerHTML = response.following
+
+        document.querySelector(".public_reps").innerHTML = response.public_repos
+
+
+    })
+}
+
+document.querySelector(".search").addEventListener("focusout", gitHubApi);
+
+
