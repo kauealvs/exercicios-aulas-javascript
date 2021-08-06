cb7a963a4c3de7da632b283e0a823172ea811a7e
fnicomedes <fnicomedes@gmail.com>
2021-08-06 16:42:06 -0300

    fix gitapi

--- exercicios/FetchAPI/apiGitHub/github.js
+++ exercicios/FetchAPI/apiGitHub/github.js
@@ -4,37 +4,35 @@
 
 //deve trazer o avatar_url, login, followers, following, public_repos, name
 
-
-
-let icon = document.querySelector(".avatar")
+let icon = document.querySelector(".avatar");
 
 const gitHubApi = () => {
-    let searchUser = document.querySelector(".search").value;
-    let urlGit = `https://api.github.com/users/${searchUser}`;
+  let urlGit = `https://api.github.com/users/${searchUser.value}`;
+  console.log(searchUser);
+  fetch(urlGit)
+    .then((r) => r.json())
+    .then((response) => {
+      console.log(response);
+      console.log(urlGit);
 
-    fetch(urlGit).then(r => r.json()).then(response => {
-        console.log(response)
-        console.log(urlGit)
-        
+      document.querySelector(".avatar").innerHTML = response.avatar_url;
 
-        document.querySelector(".avatar").innerHTML = response.avatar_url
+      icon.setAttribute("src", `${response.avatar_url}`);
 
-        icon.setAttribute("src", `${response.avatar_url}`)
+      document.querySelector(".login").innerHTML = response.login;
 
-        document.querySelector(".login").innerHTML = response.login
+      document.querySelector(".location").innerHTML = response.location;
 
-        document.querySelector(".location").innerHTML = response.location
-       
-        document.querySelector(".followers").innerHTML = response.followers
+      document.querySelector(".followers").innerHTML = response.followers;
 
-        document.querySelector(".following").innerHTML = response.following
+      document.querySelector(".following").innerHTML = response.following;
 
-        document.querySelector(".public_reps").innerHTML = response.public_repos
+      document.querySelector(".public_reps").innerHTML = response.public_repos;
+    });
+};
 
+let searchUser = document.querySelector(".search");
 
-    })
-}
+searchUser.addEventListener("focusout", gitHubApi);
 
-document.querySelector(".search").addEventListener("focusout", gitHubApi);
-
-
+// document.querySelector(".search").addEventListener("focusout", gitHubApi);
